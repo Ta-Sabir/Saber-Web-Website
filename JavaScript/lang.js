1d9a7598b6fd4963907653b964c29534926628d6
Talhaoui Saber <115227518+Ta-Sabir@users.noreply.github.com>
2023-01-17 17:11:31 +0100

    Add files via upload

---
+++ JavaScript/lang.js
@@ -0,0 +1,296 @@
+// transition Language
+let transition = {
+  ar: {
+    // Start Header
+    h3logo: "صابر ويب",
+    alanding: "الرئيسية",
+    awhysaber: "لماذا نحن ؟",
+    awork: "كيف نعمل ؟",
+    aregist: "كيف أسجل ؟",
+    aserv: "خدماتنا",
+    aportfolio: "معرض أعمالنا",
+    ateam: "فريقنا",
+    atest: "أراء عملائنا",
+    aprice: "الأسعار",
+    acontact: "تواصل معنا",
+    // Start Landing Page
+    h3Landing:
+      " <span>صابر ويب</span> خيارك لتصميم و إنشاء المواقع الإلكترونية !",
+    pLanding:
+      "الآن حان الوقت لتتخذ القرار بإنشاء موقعك الإلكتروني الخاص بمجال عملك . ماذا تنتظر وفر الجهد و قم بتصميم و إنشاء موقعك",
+    // Start Number Section
+    Requests: "الطلبات",
+    clients: "العملاء",
+    partners: "الشركاء",
+    site: "موقع إلكتروني",
+    designui: "تصميم UI/UX",
+    // Start Why Saber Web
+    titlewhysab: "لماذا صابر ويب ؟",
+    pwhysab: "لماذا تختار موقع صابر ويب لخدمتك ؟",
+    h3push: "طرق دفع متعددة",
+    ppush:
+      "توفر عدة طرق للدفع لجميع العملاء في مختلف بقاع العالم من خلال الطرق البنكية التقليدية أو عن طريق البنوك الاٍلكترونية , أو من خلال الفيزا",
+    h3money: "إسترداد النقود",
+    pmoney:
+      "يمكنك طلب استرداد الأموال خلال 7 أيام من تاريخ الشراء , إذا كان الموقع الإلكتروني لا يعمل، يمكنك طلب استرداد المبالغ المدفوعة في أي وقت",
+    h3speed: "سرعة الموقع",
+    pspeed:
+      "نحن نقدم أفضل تجربة أداء يشعر بها المستخدم من الهدوء والإستقرار داخل موقعك مع تلبية طلبك في أسرع وقت مع الاٍطلاع على المشروع خطوة بخطوة",
+    // Start How Do We Work
+    titlework: "كيف نعمل في صابر ويب ؟",
+    pwork: "تعرف كيف يعمل موقع صابر ويب",
+    titlepackage: "إختيار الباقة والمنصة",
+    ppackage:
+      "تبدأ خدماتنا من عند العميل حيث يختار الباقة المناسبة لخدماته ولغة البرمجة والمنصة التي سيتم تنفيذ تصميم الموقع عليها",
+    titleanalysis: "التحليل الأولي و تحديد السعر",
+    panalysis:
+      "العمل على تحليل كافة التفاصيل مع العميل للوصول بها إلى كافة إحتياجات وطموحات العميل من أجل تحقيقها على الواقع",
+    titleproject: "تنفيذ المشروع",
+    pproject:
+      "العمل على تصميم واجهة الإستخدام وتجربة المستخدم مع الاهتمام الكامل بإحترافية التصميم ودقة إختيار الألوان",
+    // Start How do I register
+    titleregister: "كيف أسجل ؟",
+    pregister: "تعرف على كيفية التسجيل في موقع صابر ويب",
+    h3howreg: "هيا بنا بضع خطوات سهلة للتسجيل في صابر ويب !",
+    phowreg: "إتبع الخطوات للتسجيل للإنطلاق في البحث عن عملاء",
+    reg1: "قم بالتسجيل أولا و ذلك باٍنشاء حساب جديد بملئ المعلومات المطلوبة",
+    reg2: "قم بتأكيد هويتك مع ملئ معلومات ملفك الشخصي",
+    reg3: "إنتظر قبول حسابك من فريق الدعم",
+    reg4: "بعد قبول حسابك إنطلق في البحث عن عملاء للعمل معهم",
+    // Start Our services Section
+    titleserv: "خدماتنا",
+    pserv: "تعرف على خدماتنا",
+    serv1: "تصميم المواقع",
+    booknow: "احجز الآن",
+    serv2: "برمجة المتاجر الالكترونية",
+    serv3: "برمجة صفحات الهبوط",
+    serv4: "تصميم صفحات الهبوط",
+    serv5: "تصميم وبرمجة التطبيقات",
+    serv6: "تصميم شعار وهوية",
+    // Start Portfolio Section
+    titleportf: "معرض أعمالنا",
+    pportf: "من أعمالنا في تصميم و اٍنشاء المواقع",
+    port1: 'موقع " أكاديمية مُعلم " منصة تعليمية إفتراضية',
+    port2: "تصميم و إنشاء موقع مطعم (Fast Food)",
+    port3: "تصميم و اٍنشاء صفحة هبوط ",
+    tools: "التقنيات المستعملة:",
+    // Start Team Section
+    titleteam: "فريقنا",
+    pteam: "تعرف على أعضاء فريقنا الرائعين",
+    team1: "طلحاوي صابر - مطور واجهات الويب",
+    team2: "طلحاوي صابر - مصمم UI / UX",
+    team3: "طلحاوي صابر - مطور ويب",
+    team4: "طلحاوي صابر - مطور ألعاب",
+    // Satrt Testimonials Section
+    titletest: "أراء عملائنا",
+    ptest: "شاهد أراء عملائنا",
+    ptest1:
+      "من أفضل المواقع في تصميم و اٍنشاء المواقع ، نشكر الإدارة ساعدتنا كثيرا ومعاملتهم في غاية الراحة",
+    ptest2:
+      "موقع صابر ويب من أفضل مواقع التصميم و برمجة المواقع أنصح الجميع بالتعامل معهم يتميزون بالعمل المتقن",
+    test3: "طلحاوي صابر - صاحب متجر اٍلكتروني",
+    ptest3:
+      "أنا سعيد جدا بالتعامل مع موقع صابر ويب لتصميم و اٍنشاء المواقع لقد نفذوا ماطلبته بكل احترافية",
+    // Start prices Section
+    titleprices: "الأسعار",
+    pprices: "نوفر مجموعة من الباقات تناسب جميع العملاء لدينا",
+    year: "سنوي",
+    month: "شهري",
+    liprice1: "دومين مجانا",
+    liprice2: "استضافة <bdi>900</bdi> ميجا",
+    liprice3: "البريد الالكتروني <bdi>10</bdi>",
+    liprice4: "الموقع <bdi>10</bdi> صفحات",
+    liprice5: "لغة واحدة عربي أو انجليزي",
+    liprice6: "متوافق مع جميع أحجام الشاشات",
+    liprice7: "ربط الموقع بمواقع التواصل",
+    liprice8: "تهيئة الموقع لمحركات البحث",
+    liprice9: "لوحة تحكم للموقع",
+    personal: "شخصي",
+    mutual: "مشترك",
+    // Start Download Section
+    titledown: "حمل تطبيق صابر ويب الآن",
+    pdown:
+      "إعرف التكلفة الإجمالية لطلبك شاملة الشحن والتوصيل، ثم أتمم عملية الشراء وإدفع الطلب بواسطة وسائل الدفع المتاحة على تطبيقنا",
+    btndown: "حمله الان من",
+    // Start Blog Section
+    titleblog: "مدونة صابر ويب",
+    pblog: "تعرف أكثر على موقع صابر ويب من خلال المدونة",
+    hblog1: "ماهو موقع صابر ويب ؟",
+    hblog2: "من هو منشئ موقع صابر ويب ؟",
+    hblog3: "كيف يمكنني التواصل مع صابر ويب",
+    hblog4: "ماهي وسائل الدفع في صابر ويب ؟",
+    spblog: "01/01/2023 . 3 مليون مشاهدة",
+    // Start Contact Me Section
+    titlecontact: "تواصل معنا",
+    pcontact:
+      "نحن في انتظارك طول الوقت يمكنك ارسال الرسالة وسيتم الرد عليك خلال اليوم",
+    usercontac:
+      '<input class="input" type="text" data-lang="usercontac" placeholder="إسم المستخدم"/>',
+    emailcontact:
+      '<input class="input" type="email" placeholder="عنوان البريد الالكتروني"/>',
+    messagecontac:
+      '<input class="message" type="text" placeholder="عنوان الرسالة"/>',
+    textreacontact:
+      '<textarea style="margin-top: 18px" class="my-input" cols="30" rows="10" placeholder="الرسالة"></textarea>',
+    btnsend: "اٍرسال",
+    socialmedia: "مواقع التواصل الاجتماعي",
+    // Start Footer Section
+    ptitlefooter:
+      "يسعى موقع صابر ويب إلى تغيير مفهوم تصميم المواقع وبرمجة المواقع بشكل خاص وحلول المواقع بشكل عام وذلك من خلال تقديم خدمة متنوعة وعروض مناسبة لجميع الأفراد والشركات بأحدث التقنيات وأفضل جودة وأسرع وقت مع كل ذلك تأتي الأمانة والصدق والوضوح مع العملاء مع إمكانية تقسيط الدفع",
+    linksfooter: "روابط مختصرة",
+    titletwasol: "تواصل معنا",
+    algeriaschl: "الجزائر . ولاية الشلف",
+    timework: "أوقات العمل : كل يوم / 24 ساعة",
+    copyright: "جميع الحقوق محفوظة لموقع صابر ويب © 2023",
+    emailright: '<input type="search" placeholder="أدخل بريدك الإلكتروني" />',
+    sendright: '<input type="submit" value="إرسال" />',
+    madewith: "تم تصميم و إنشاء هذا الموقع من طرف طلحاوي صابر 💜",
+  },
+  eng: {
+    // Start Header
+    alanding: "Home",
+    awhysaber: "Why Us ?",
+    awork: "How Do We Work",
+    aregist: "How do I Register ?",
+    aserv: "Services",
+    aportfolio: "Our Business Gallery",
+    ateam: "Our Team",
+    atest: "Testimonials",
+    aprice: "Prices",
+    acontact: "Conact Us",
+    // Start Landing
+    h3Landing:
+      "<span>Saber Web</span> is your choice for designing and creating websites !",
+    pLanding:
+      "Now it's time to make the decision to create your own business website. What are you waiting for Save effort and design and create your site .",
+    h3logo: "Saber Web",
+    // Start Number Section
+    Requests: "Requests",
+    clients: "Clients",
+    partners: "Partners",
+    site: "Site Web",
+    designui: "Design UI/UX",
+    // Start Why Saber Web
+    titlewhysab: "Why Saber Web?",
+    pwhysab: "Why choose Saber Web site to serve you ?",
+    h3push: "Multiple payment methods",
+    ppush:
+      "It provides several methods of payment to all customers in various parts of the world through traditional banking methods or through electronic banks, or through Visa",
+    h3money: "Cashback",
+    pmoney:
+      "You can request a refund within 7 days of the date of purchase. If the website is not working, you can request a refund at any time.",
+    h3speed: "Site speed",
+    pspeed:
+      "We provide the best performance experience that the user feels calm and stable within your site, while fulfilling your request in the fastest time, while reviewing the project step by step",
+    // Start How Do We Work
+    titlework: "How do we work in Saber Web ?",
+    pwork: "Learn how Saber Web works",
+    titlepackage: "Choose the package and platform",
+    ppackage:
+      "Our services start with the client, where he chooses the appropriate package for his services, the programming language, and the platform on which the website design will be implemented",
+    titleanalysis: "Preliminary analysis and pricing",
+    panalysis:
+      "Work on analyzing all the details with the client to reach all the needs and aspirations of the client in order to achieve them in reality",
+    titleproject: "Project execution",
+    pproject:
+      "Work on designing the user interface and user experience with full attention to the professionalism of the design and the accuracy of the choice of colors",
+    // Start How do I register
+    titleregister: "How do I register ?",
+    pregister: "Find out how to register on the Saber Web site",
+    h3howreg: "Let's take a few easy steps to register on Saber Web !",
+    phowreg: "Follow the steps to register to start searching for clients",
+    reg1: "Register first by creating a new account and filling in the required information",
+    reg2: "Confirm your identity and fill in your profile information",
+    reg3: "Wait for your account to be approved by the support team",
+    reg4: "After accepting your account, start searching for clients to work with",
+    // Start Our services Section
+    titleserv: "Our Services",
+    pserv: "Learn about our services",
+    serv1: "Sites Design",
+    booknow: "Book Now",
+    serv2: "Programming electronic stores",
+    serv3: "Programming landing pages",
+    serv4: "Landing page design",
+    serv5: "Application design and programming",
+    serv6: "Logo and identity design",
+    // Start Portfolio Section
+    titleportf: "Our business gallery",
+    pportf: "One of our works in designing and creating websites",
+    port1: 'The "Teacher Academy" website is a virtual educational platform',
+    port2: "Design and create a restaurant website (Fast Food)",
+    port3: "Design and build a landing page with several sections",
+    tools: "Technologies used:",
+    // Start Team Section
+    titleteam: "Our Team",
+    pteam: "Meet our amazing team members",
+    team1: "Talhaoui Saber - Frontend Dev",
+    team2: "Talhaoui Saber - UI/UX Designer",
+    team3: "Talhaoui Saber - Web Developer",
+    team4: "Talhaoui Saber - Game Developer",
+    // Satrt Testimonials Section
+    titletest: "Our customers' opinions",
+    ptest: "See what our customers say",
+    ptest1:
+      "One of the best websites in designing and creating websites. We thank the administration for helping us a lot and treating them very comfortably",
+    ptest2:
+      "Saber Web is one of the best websites for designing and programming websites. I advise everyone to deal with them. They are distinguished by their excellent work",
+    test3: "Talhaoui Saber - Owner of an online store",
+    ptest3:
+      "I am very happy to deal with the Saber Web site for designing and creating websites. They have implemented what I asked for with all professionalism.",
+    // Start prices Section
+    titleprices: "The Prices",
+    pprices: "We offer a range of packages to suit all of our customers",
+    year: "annual",
+    month: " Monthly",
+    liprice1: "Free Domain",
+    liprice2: "hosting<bdi>900</bdi>Mega",
+    liprice3: "Email 10",
+    liprice4: "The site is 10 pages long",
+    liprice5: "One language Arabic or English",
+    liprice6: "Compatible with all screen sizes",
+    liprice7: "Linking the site to communication",
+    liprice8: "Configuring the site for search",
+    liprice9: "Website control panel",
+    personal: "Personal",
+    mutual: "Mutual",
+    // Start Download Section
+    titledown: "Download the Saber Web application now",
+    pdown:
+      "Know the total cost of your order, including shipping and delivery, then complete the purchase process and pay the order using the payment methods available on our application",
+    btndown: "Download now from",
+    // Start Blog Section
+    titleblog: "Saber Web Blog",
+    pblog: "Learn more about the Saber Web site through the blog",
+    hblog1: "What is Saber Web site ?",
+    hblog2: "Who is the creator of the Site ?",
+    hblog3: "How can I contact Saber Webb ?",
+    hblog4: "What are the payment methods ?",
+    spblog: "01/01/2023 . 3 million views",
+    // Start Contact Me Section
+    titlecontact: "Contact Us",
+    pcontact:
+      "We are waiting for you all the time. You can send the message and we will reply to you within the day",
+    usercontac:
+      '<input class="input" type="text" data-lang="usercontac" placeholder="User Name"/>',
+    emailcontact:
+      '<input class="input" type="email" placeholder="E-mail address"/>',
+    messagecontac:
+      '<input class="message" type="text" placeholder="Title of Message"/>',
+    textreacontact:
+      '<textarea style="margin-top: 18px" class="my-input" cols="30" rows="10" placeholder="Message"></textarea>',
+    btnsend: "Send",
+    socialmedia: "Social Media",
+    // Start Footer Section
+    ptitlefooter:
+      "Saber Web seeks to change the concept of website design and website programming in particular, and website solutions in general, by providing a variety of services and suitable offers for all individuals and companies with the latest technology, the best quality and the fastest time.",
+    linksfooter: "Short Links",
+    titletwasol: "Contact Us",
+    algeriaschl: "Algeria . Wilaya De Chlef",
+    timework: "Working hours: every day / 24 hours",
+    copyright: "All rights reserved to Saber Web © 2023",
+    emailright:
+      '<input type="search" placeholder="Enter your email address" />',
+    sendright: '<input type="submit" value="Send" />',
+    madewith: "This website is designed and created by Talhaoui Saber 💜",
+  },
+};
